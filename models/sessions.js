4756c3fa27363728d6b18e87360c27d64dc59377
Dimkajasons <dimkajasons@gmail.com>
2018-05-30 00:26:42 +0300

    add chain of inheritance for sessions

---
+++ models/sessions.js
@@ -0,0 +1,11 @@
+class SessionCollection extends Collection {
+    constructor (options) {
+        super (options);
+    }
+
+}
+class SessionModel extends Model {
+    constructor (options){
+        super(options);
+    }
+}
